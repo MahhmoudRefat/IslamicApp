207845decd4622abd5ba4499eea21f4de98d00ef
mahhmoud refaat <mahhmoudrefat@outlook.com>
2024-01-19 08:47:57 +0200

    start creating the app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.islamicapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.islamicapp"
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures{
+        viewBinding=true
     }
 }
 
--- app/src/main/java/com/example/islamicapp/MainActivity.kt
+++ app/src/main/java/com/example/islamicapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.islamicapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.DocumentsContract.Root
+import androidx.fragment.app.Fragment
+import com.example.islamicapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
     }
+
 }
