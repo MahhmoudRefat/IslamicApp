8e4c0a2ac183a5985ffc7321c9993b7ece86102e
mahhmoud refaat <mahhmoudrefat@outlook.com>
2024-02-16 21:16:19 +0200

    fix :  radio data

--- app/src/main/java/com/example/islamicapp/api/sourcesResponse/Radio.kt
+++ app/src/main/java/com/example/islamicapp/api/sourcesResponse/Radio.kt
@@ -3,5 +3,6 @@ package com.example.islamicapp.api.sourcesResponse
 data class Radio(
     val id: Int?=null,
     val name: String?=null,
-    val url: String?=null
+    val url: String?=null,
+    val url2: String?=null
 )
